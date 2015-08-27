f6cc99e2133a1f65358b0805e04ec6a1aacdb314
ginovski <sashko.ginovski@gmail.com>
2015-08-27 16:13:40 +0300

    Created main index.html file

--- app/config/config.js
+++ app/config/config.js
@@ -17,9 +17,14 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
     "core-js": "npm:core-js@0.9.18",
+    "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
     },
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
