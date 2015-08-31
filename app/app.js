2d6fb3cef7fae0f72d1b65b28c42d5fe45586cc9
IvanRPanchev <ivan.r.panchev@gmail.com>
2015-08-31 20:45:30 +0300

    Added titles page layout

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 import sammy from 'sammy'
 
+import BooksController from 'app/controllers/BooksController'
 import HomeController from 'app/controllers/HomeController'
 import UsersController from 'app/controllers/UsersController'
 
 var app = Sammy('#main', function() {
-    //this.get('#/', HomeController.index);
+    this.get('#/', HomeController.index);
     this.get('#/login', UsersController.login);
     this.get('#/register', UsersController.register);
+    this.get('#/titles', BooksController.titles)
 });
 
 app.run('#/');
