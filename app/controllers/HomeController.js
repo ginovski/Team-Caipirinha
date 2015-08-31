48188169c4069f7ab53e40ae9c8610fce5a8803a
ginovski <sashko.ginovski@gmail.com>
2015-08-31 17:33:03 +0300

    Removed bookModel references

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,14 +1,11 @@
 import htmlRenderer from 'app/views/helpers/htmlRenderer'
 import partialsHelper from 'app/views/helpers/partialsHelper'
 
-import booksModel from 'app/models/booksModel'
-
 var HomeController = (function(){
     function index(){
         partialsHelper
             .getPartialViewTemplate('index')
             .then(function(template){
-
                 partialsHelper.addPartialToPage(template);
             });
     }
