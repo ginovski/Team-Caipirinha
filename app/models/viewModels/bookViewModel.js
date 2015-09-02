c386718d9df8fafe8d72cba2b3e33335fec29dbe
Spurch <ivan.stoyanov.nikolov@gmail.com>
2015-09-02 18:04:26 +0300

    Added ISBN number validator
    Note that this validator will work for both the old (10-digit standard)
    and the new (13-digit standard). We can pass the input value as string
    with or without dashes between the digits.

--- app/models/viewModels/bookViewModel.js
+++ app/models/viewModels/bookViewModel.js
@@ -96,7 +96,7 @@ class Book {
     }
 
     set setIsbn(value) {
-        if (typeof value !== number || value.length !== 13) {
+        if (!isbnValidator(value)) {
             alert('ISBN should be 13 digits');
         } else {
             this.isbn = value;
@@ -113,6 +113,37 @@ class Book {
         this.bookUrl = value;
         return this;
     }
+
+    function isbnValidator(value){
+        var isbnValue = value.toString().toLowerCase().replace(/-/g, "");
+
+        if(isbnValue.match(/[a-z]/i) || isbnValue.length != 13 && isbnValue.length != 10) {
+            console.log(isbnValue.length);
+            return false;
+        }
+        if(isbnValue.length == 10){
+            isbnValue = "978" + isbnValue;
+        }
+
+        var i,
+            mod,
+            sum = 0;
+        len = isbnValue.length-1;
+        for(i = 0;  i < len; i+=1){
+            if(i%2 == 0){
+                sum += parseInt(isbnValue.charAt(i), 10)*1;
+            } else {
+                sum += parseInt(isbnValue.charAt(i), 10)*3;
+            }
+        }
+
+        mod = sum%10;
+        if(10 - mod !== parseInt(isbnValue.charAt(12))){
+            return false;
+        }
+
+        return true;
+    };
 }
 
 export default Book;
